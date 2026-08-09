a48b27427307b680bd9f60030c5185dd5547ca0b
ailinmartinez365-jpg <ailinmartinez365@gmail.com>
2026-08-09 02:14:14 -0600

    app.py

--- app.py
+++ app.py
@@ -4,12 +4,21 @@ st.title("🍃 ZeroWaste Chef")
 
 st.write("Convierte lo que tienes en lo que puedes cocinar.")
 
-ingredientes = st.text_input(
+texto_ingredientes = st.text_input(
     "¿Qué ingredientes tienes?",
     placeholder="Ejemplo: huevo, tomate, queso"
 )
 
-if ingredientes:
-    st.write("Tus ingredientes son:", ingredientes)
+if texto_ingredientes:
+
+    ingredientes = texto_ingredientes.lower().split(",")
+
+    ingredientes = [ingrediente.strip() for ingrediente in ingredientes]
+
+    st.write("Tus ingredientes son:")
+
+    for ingrediente in ingredientes:
+        st.write("🥕", ingrediente)
+
 else:
     st.warning("Por favor, introduce al menos un ingrediente.")
