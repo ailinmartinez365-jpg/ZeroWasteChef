d4d17f587fb514c4990a862a80eb5e4c9ac4f87d
ailinmartinez365-jpg <ailinmartinez365@gmail.com>
2026-08-09 02:10:05 -0600

    app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title("🍃 ZeroWaste Chef")
 
 st.write("Convierte lo que tienes en lo que puedes cocinar.")
+
+ingredientes = st.text_input(
+    "¿Qué ingredientes tienes?",
+    placeholder="Ejemplo: huevo, tomate, queso"
+)
+
+if ingredientes:
+    st.write("Tus ingredientes son:", ingredientes)
+else:
+    st.warning("Por favor, introduce al menos un ingrediente.")
