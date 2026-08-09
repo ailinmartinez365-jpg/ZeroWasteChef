973baccb6ceebd6925b162720fdaa283784674ca
ailinmartinez365-jpg <ailinmartinez365@gmail.com>
2026-08-09 02:43:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,9 @@ texto_ingredientes = st.text_input(
     "¿Qué ingredientes tienes?",
     placeholder="Ejemplo: huevo, tomate, queso"
 )
-
-if texto_ingredientes:
-
-    ingredientes = texto_ingredientes.lower().split(",")
-
-    ingredientes = [ingrediente.strip() for ingrediente in ingredientes]
-
-    st.write("Tus ingredientes son:")
-
-    for ingrediente in ingredientes:
-        st.write(ingrediente)
     st.subheader("Recetas encontradas")
+
+    resultados = []
 
     for receta in recetas:
 
@@ -33,8 +24,35 @@ if texto_ingredientes:
 
         porcentaje = (coincidencias / len(receta["ingredientes"])) * 100
 
+        if porcentaje > 0:
+            resultados.append({
+                "receta": receta,
+                "porcentaje": porcentaje
+            })
+
+    resultados.sort(
+        key=lambda resultado: resultado["porcentaje"],
+        reverse=True
+    )
+
+    for resultado in resultados:
+
+        receta = resultado["receta"]
+        porcentaje = resultado["porcentaje"]
+
         st.write(receta["nombre"])
         st.write("Coincidencia:", porcentaje, "%")
+if texto_ingredientes:
+
+    ingredientes = texto_ingredientes.lower().split(",")
+
+    ingredientes = [ingrediente.strip() for ingrediente in ingredientes]
+
+    st.write("Tus ingredientes son:")
+
+    for ingrediente in ingredientes:
+        st.write(ingrediente)
+    
 
     st.write("Cantidad de recetas disponibles:", len(recetas))
 
