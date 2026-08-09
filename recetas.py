bf8408344f8037e9dfe476cb4df0778bc2060fd7
ailinmartinez365-jpg <ailinmartinez365@gmail.com>
2026-08-09 02:22:48 -0600

    Create recetas.py

---
+++ recetas.py
@@ -0,0 +1,73 @@
+recetas = [
+
+    {
+        "nombre": "Huevos con tomate",
+        "ingredientes": ["huevo", "tomate", "sal", "aceite"],
+        "tiempo": 10,
+        "nivel": "Principiante",
+        "instrucciones": [
+            "Calienta un sartén.",
+            "Agrega un poco de aceite.",
+            "Añade el tomate picado.",
+            "Agrega los huevos y la sal.",
+            "Cocina hasta que los huevos estén listos."
+        ]
+    },
+
+    {
+        "nombre": "Quesadillas de queso",
+        "ingredientes": ["tortilla", "queso", "aceite"],
+        "tiempo": 10,
+        "nivel": "Principiante",
+        "instrucciones": [
+            "Calienta un sartén.",
+            "Coloca una tortilla.",
+            "Agrega el queso.",
+            "Dobla la tortilla.",
+            "Cocina hasta que el queso se derrita."
+        ]
+    },
+
+    {
+        "nombre": "Pasta con tomate",
+        "ingredientes": ["pasta", "tomate", "ajo", "aceite", "sal"],
+        "tiempo": 20,
+        "nivel": "Explorador",
+        "instrucciones": [
+            "Cocina la pasta.",
+            "Calienta aceite en un sartén.",
+            "Agrega el ajo y el tomate.",
+            "Añade la pasta.",
+            "Mezcla y agrega sal al gusto."
+        ]
+    },
+
+    {
+        "nombre": "Arroz con verduras",
+        "ingredientes": ["arroz", "zanahoria", "cebolla", "aceite", "sal"],
+        "tiempo": 25,
+        "nivel": "Explorador",
+        "instrucciones": [
+            "Cocina el arroz.",
+            "Pica las verduras.",
+            "Sofríe la cebolla y la zanahoria.",
+            "Agrega el arroz.",
+            "Mezcla todos los ingredientes."
+        ]
+    },
+
+    {
+        "nombre": "Pasta cremosa especial",
+        "ingredientes": ["pasta", "queso", "leche", "ajo", "mantequilla"],
+        "tiempo": 35,
+        "nivel": "Experto",
+        "instrucciones": [
+            "Cocina la pasta.",
+            "Derrite la mantequilla.",
+            "Agrega el ajo.",
+            "Añade la leche y el queso.",
+            "Incorpora la pasta.",
+            "Mezcla hasta obtener una salsa cremosa."
+        ]
+    }
+]
