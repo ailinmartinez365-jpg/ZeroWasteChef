1b2c7da6cca3a643648efe9d848a72ec0d54a093
ailinmartinez365-jpg <ailinmartinez365@gmail.com>
2026-08-15 01:56:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -295,43 +295,45 @@ if ingredientes_usuario:
                                 "Nivel:",
                                 receta["nivel"]
                             )
-                                    if st.button(
-                                        "Ver receta",
-                                        key="ver_" + receta["nombre"]
+                                
+                                if st.button(
+                                    
+                                    "Ver receta",
+                                    key="ver_" + receta["nombre"]
+                                ):
+                                
+                                    st.divider()
+                                
+                                    st.subheader(
+                                        "🍳 " + receta["nombre"]
+                                    )
+                                
+                                    st.write(
+                                        "### Ingredientes"
+                                    )
+                                
+                                    for ingrediente in receta["ingredientes"]:
+                                
+                                        st.write(
+                                            "•",
+                                            ingrediente
+                                        )
+                                
+                                    st.write(
+                                        "### Preparación"
+                                    )
+                                
+                                    for numero, instruccion in enumerate(
+                                        receta["instrucciones"],
+                                        start=1
                                     ):
-                                    
-                                        st.divider()
-                                    
-                                        st.subheader(
-                                            "🍳 " + receta["nombre"]
-                                        )
-                                    
+                                
                                         st.write(
-                                            "### Ingredientes"
+                                            str(numero) + ".",
+                                            instruccion
                                         )
-                                    
-                                        for ingrediente in receta["ingredientes"]:
-                                    
-                                            st.write(
-                                                "•",
-                                                ingrediente
-                                            )
-                                    
-                                        st.write(
-                                            "### Preparación"
-                                        )
-                                    
-                                        for numero, instruccion in enumerate(
-                                            receta["instrucciones"],
-                                            start=1
-                                        ):
-                                    
-                                            st.write(
-                                                str(numero) + ".",
-                                                instruccion
-                                            )
-                                    
-                                        st.divider()
+                                
+                                    st.divider()
 
 
     else:
